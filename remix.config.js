ad65b2bdc80d75c944e5cfdbe040dfe8385b8cfc
Mohammed 'Mo' Mulazada <mohammed.mulazada@hva.nl>
2021-12-19 12:09:57 +0100

    2

--- remix.config.js
+++ remix.config.js
@@ -3,8 +3,8 @@
  */
 module.exports = {
   appDirectory: "app",
-  assetsBuildDirectory: "public/build",
+  browserBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "api/_build",
+  serverBuildDirectory: "build",
   ignoredRouteFiles: [".*"],
 };
